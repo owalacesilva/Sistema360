515971b887da2e0190bec829a6ae5b901d7125de
Walace Silva <wsilva.emp@gmail.com>
2021-11-11 00:19:46 +0000

    style: adicionado pagina home (template)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  # The path used after sign in.
+  def after_sign_in_path_for(resource)
+    backoffice_dashboard_path
+  end
 end

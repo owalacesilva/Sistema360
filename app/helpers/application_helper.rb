90f51f79e76ac07641a574ba22f749380349c157
Walace Silva <wsilva.emp@gmail.com>
2021-12-08 23:12:13 -0300

    Criado faker produtos e corrigicao de bugs (#12)
    * feat(store): foi criado faker produtos no seed
    
    * refator(store): pagina de produtos foi melhorada
    
    * fix(tree): correçao de bug nos controllers de rede
    
    * feat: presenter para produtos foi adicionado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def presenter(model)
+    klass = "#{model.class}Presenter".constantize
+    instance = klass.new(model)
+    yield(instance) if block_given?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'factory_bot_rails'
 
 # Root member
 root_attr = FactoryBot.attributes_for(:user, :root, :actived, :verified)
@@ -13,8 +12,15 @@ root = User.new(root_attr)
 root.save!
 
 # Childrens from root
-(1..50).each do 
+50.times do
   user_attr = FactoryBot.attributes_for(:user, :actived, :verified, sponsor: root)
   user = User.new(user_attr)
   user.save!
+end
+
+# Faker products
+10.times do
+  product_attr = FactoryBot.attributes_for(:product, :published)
+  product = Product.new(product_attr)
+  product.save!
 end

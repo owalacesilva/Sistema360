2dfc165d0d6dc1ea7186e8ae03b1ebeddce57a16
Walace Silva <wsilva.emp@gmail.com>
2021-12-18 17:43:17 +0000

    refactor: adicionado alguns campos de referencia no modelo user_point_record

--- db/migrate/20211112213605_create_user_point_records.rb
+++ db/migrate/20211112213605_create_user_point_records.rb
@@ -6,13 +6,16 @@ class CreateUserPointRecords < ActiveRecord::Migration[6.1]
 
       # References
       t.references :user, null: false, foreign_key: { to_table: :users, on_delete: :restrict, on_update: :cascade }
+      t.references :origin_user, null: false, foreign_key: { to_table: :users, on_delete: :restrict, on_update: :cascade }
       t.references :reference, null: false, foreign_key: { to_table: :references, on_delete: :restrict, on_update: :cascade }
 
       # Custom fields
       t.date :record_date
+      t.decimal :base_value, precision: 10, scale: 2
       t.decimal :amount, precision: 10, scale: 2
     end
   end
+
 
   def down
     drop_table :user_point_records

38dc61ea980b7785d49cdb57305430a16005954d
Walace Silva <wsilva.emp@gmail.com>
2022-01-12 19:08:37 +0000

    feat: criado metodo para buscar ou criar o carrinho em seção

--- app/controllers/backoffice/backoffice_controller.rb
+++ app/controllers/backoffice/backoffice_controller.rb
@@ -1,4 +1,22 @@
 class Backoffice::BackofficeController < ApplicationController
   before_action :authenticate_user!
+  before_action :current_cart
 
+  private
+
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find(session[:cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:cart_id] = nil
+      end
+    end
+
+    if session[:cart_id] = nil
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  end
 end

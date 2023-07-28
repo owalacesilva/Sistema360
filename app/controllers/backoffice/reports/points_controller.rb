e544c78577db94230572976fe2fd047f4c8f2445
Walace Silva <wsilva.emp@gmail.com>
2023-07-28 20:01:45 -0300

    feat: adicionado filtro na pagina de relatório de pontos

--- app/controllers/backoffice/reports/points_controller.rb
+++ app/controllers/backoffice/reports/points_controller.rb
@@ -1,7 +1,9 @@
 class Backoffice::Reports::PointsController < Backoffice::BackofficeController
 
   def index
+    @filters = filter_params('points_report')
+    query = PointsReportQuery.call(relation: current_user, filters: @filters)
     @point = current_user.point(:activation)
-    @records = current_user.point_records.order(created_at: :desc)
+    @records = query.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
